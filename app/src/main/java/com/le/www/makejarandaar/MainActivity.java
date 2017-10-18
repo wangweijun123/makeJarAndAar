d622b30589573d9538bd473865069968a3813c45
wangweijun1 <wangweijun1@le.com>
2017-10-18 17:01:26 +0800

    add test module

--- app/src/main/java/com/le/www/makejarandaar/MainActivity.java
+++ app/src/main/java/com/le/www/makejarandaar/MainActivity.java
@@ -3,11 +3,17 @@ package com.le.www.makejarandaar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.le.www.toollibrary.ToolLibrary;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 使用module中的类
+        ToolLibrary.test();
+        // 使用module中资源
+        String s = getString(R.string.test);
     }
 }
---
+++ toollibrary/src/main/java/com/le/www/toollibrary/ToolLibrary.java
@@ -0,0 +1,13 @@
+package com.le.www.toollibrary;
+
+import android.util.Log;
+
+/**
+ * Created by wangweijun1 on 2017/10/18.
+ */
+
+public class ToolLibrary {
+    public static void test() {
+        Log.i("ToolLibrary", "test");
+    }
+}
