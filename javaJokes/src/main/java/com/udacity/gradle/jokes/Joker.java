9818129ad4dfb281fd6e15eaea48683c8cee9d34
wangweijun1 <wangweijun1@le.com>
2017-10-20 10:10:43 +0800

    java plugin for gradle  jdk1.7

--- javaJokes/src/main/java/com/udacity/gradle/jokes/Joker.java
+++ javaJokes/src/main/java/com/udacity/gradle/jokes/Joker.java
@@ -4,4 +4,8 @@ public class Joker {
     public String getJoke() {
         return "This is totally a funny joke";
     }
+
+    public String getJoke2() {
+        return "This is totally a funny joke";
+    }
 }
